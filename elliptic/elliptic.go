ce1ff72f91ba7cc7a3851000d93752ad50dfb597
Kelby Ludwig <kelby.ludwig@praetorian.com>
2017-01-01 17:19:22 -0600

    added curve addition and doubling tests

--- elliptic/elliptic.go
+++ elliptic/elliptic.go
@@ -1,45 +1,145 @@
-package main
+package elliptic
 
 import (
 	"crypto/elliptic"
 	"math/big"
 )
 
-type genericCurve struct {
-	*CurveParams
+var zero *big.Int = big.NewInt(0)
+var one *big.Int = big.NewInt(1)
+var two *big.Int = big.NewInt(2)
+var three *big.Int = big.NewInt(3)
+
+type shortWeierstrassCurve struct {
+	*elliptic.CurveParams
+	A *big.Int
 }
 
-func NewCurve(name string, bitsize int, gx, gy, b, n, p *big.Int) (curve elliptic.Curve) {
-	curve = genericCurve{}
-	curve.Name = name
-	curve.BitSize = bitsize
-	curve.Gx = gx
-	curve.Gy = gy
-	curve.B = b
-	curve.N = n
-	curve.P = p
+//NewCurve creates a new curve that implements the `elliptic.Curve` interface
+//with custom parameters.
+func NewCurve(a, b, p, gx, gy *big.Int) (curve shortWeierstrassCurve) {
+	curveParams := elliptic.CurveParams{
+		Name:    "short weierstrass curve",
+		BitSize: 0,
+		N:       zero,
+		Gx:      gx,
+		Gy:      gy,
+		B:       b,
+		P:       p,
+	}
+	curve = shortWeierstrassCurve{
+		CurveParams: &curveParams,
+		A:           a,
+	}
 	return curve
 }
 
-func (curve genericCurve) Params() *elliptic.CurveParams {
+func (curve shortWeierstrassCurve) Params() *elliptic.CurveParams {
 	return curve.CurveParams
 }
 
-func (curve genericCurve) IsOnCurve(x, y *big.Int) bool {
+func (curve shortWeierstrassCurve) IsOnCurve(x, y *big.Int) bool {
+	panic("not implemented")
+	return false
+}
+
+func (curve shortWeierstrassCurve) Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int) {
+
+	if curve.isZeroPoint(x1, y1) {
+		x = new(big.Int).SetBytes(x2.Bytes())
+		y = new(big.Int).SetBytes(y2.Bytes())
+		return
+	}
+
+	if curve.isZeroPoint(x2, y2) {
+		x = new(big.Int).SetBytes(x1.Bytes())
+		y = new(big.Int).SetBytes(y1.Bytes())
+		return
+	}
+
+	x2i, y2i := curve.invertPoint(x2, y2)
+	if curve.PointEquals(x1, y1, x2i, y2i) {
+		x = new(big.Int).SetBytes(zero.Bytes())
+		y = new(big.Int).SetBytes(one.Bytes())
+		return
+	}
+
+	m := new(big.Int)
+	if curve.PointEquals(x1, y1, x2, y2) {
+		//m = (3*x1^2 + a) / 2*y1
+		m = m.Exp(x1, two, curve.P)
+		m = m.Mul(m, three)
+		m = m.Add(m, curve.A)
+		bot := new(big.Int).Mul(y1, two)
+		bot = bot.Mod(bot, curve.P)
+		bot = bot.ModInverse(bot, curve.P)
+		m = m.Mul(m, bot)
+		m = m.Mod(m, curve.P)
+	} else {
+		//m = (y2 - y1) / (x2 - x1)
+		m = m.Sub(y2, y1)
+		bot := new(big.Int).Sub(x2, x1)
+		bot = bot.Mod(bot, curve.P)
+		bot = bot.ModInverse(bot, curve.P)
+		m = m.Mul(m, bot)
+		m = m.Mod(m, curve.P)
+	}
+
+	x = new(big.Int).Exp(m, two, curve.P)
+	x = x.Sub(x, x1)
+	x = x.Sub(x, x2)
+	x = x.Mod(x, curve.P)
+
+	y = new(big.Int).Sub(x1, x)
+	y = y.Mul(m, y)
+	y = y.Sub(y, y1)
+	y = y.Mod(y, curve.P)
+	return
+}
+
+func (curve shortWeierstrassCurve) Double(x1, y1 *big.Int) (x, y *big.Int) {
+	return curve.Add(x1, y1, x1, y1)
+}
+
+func (curve shortWeierstrassCurve) ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int) {
+	panic("not implemented")
+	return zero, zero
+}
+
+func (curve shortWeierstrassCurve) ScalarBaseMult(k []byte) (x, y *big.Int) {
+	panic("not implemented")
+	return zero, zero
+}
+
+//isZeroPoint will return true if the supplied (x,y) values are the "zero"
+//value on a curve.
+func (curve shortWeierstrassCurve) isZeroPoint(x, y *big.Int) bool {
+
+	one := big.NewInt(1)
+	zero := big.NewInt(0)
+	return curve.PointEquals(x, y, zero, one)
 
 }
 
-func (curve genericCurve) Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int) {
+//PointEquals will return true if the supplied points (x1, y1) and (x2, y2) are
+//equal on the given curve.
+func (curve shortWeierstrassCurve) PointEquals(x1, y1, x2, y2 *big.Int) bool {
 
+	//ensure the points are reduced mod P
+	x1r := new(big.Int).Mod(x1, curve.P)
+	y1r := new(big.Int).Mod(y1, curve.P)
+	x2r := new(big.Int).Mod(x2, curve.P)
+	y2r := new(big.Int).Mod(y2, curve.P)
+
+	if x1r.Cmp(x2r) == 0 && y1r.Cmp(y2r) == 0 {
+		return true
+	}
+	return false
 }
-func (curve genericCurve) Double(x1, y1 *big.Int) (x, y *big.Int) {
 
-}
-
-func (curve genericCurve) ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int) {
-
-}
-
-func (curve genericCurve) ScalarBaseMult(k []byte) (x, y *big.Int) {
-
+//invertPoint inverts the point (x, y) on the given curve.
+func (curve shortWeierstrassCurve) invertPoint(x, y *big.Int) (xi, yi *big.Int) {
+	xi = new(big.Int).SetBytes(x.Bytes())
+	yi = new(big.Int).Sub(curve.P, y)
+	return
 }
---
+++ elliptic/elliptic_test.go
@@ -0,0 +1,69 @@
+package elliptic
+
+import (
+	"math/big"
+	"testing"
+)
+
+var a *big.Int
+var b *big.Int
+var p *big.Int
+var gx *big.Int
+var gy *big.Int
+var curve shortWeierstrassCurve
+
+func init() {
+	a, _ = new(big.Int).SetString("-95051", 10)
+	b, _ = new(big.Int).SetString("11279326", 10)
+	p, _ = new(big.Int).SetString("233970423115425145524320034830162017933", 10)
+	gx, _ = new(big.Int).SetString("182", 10)
+	gy, _ = new(big.Int).SetString("85518893674295321206118380980485522083", 10)
+	curve = NewCurve(a, b, p, gx, gy)
+}
+
+func TestCurveAddition(t *testing.T) {
+	zX := big.NewInt(0)
+	zY := big.NewInt(1)
+
+	x1, y1 := curve.Add(zX, zY, gx, gy)
+	if !curve.PointEquals(x1, y1, gx, gy) {
+		t.Errorf("adding by zero did not return original point")
+		return
+	}
+
+	x2, y2 := curve.Add(gx, gy, zX, zY)
+	if !curve.PointEquals(gx, gy, x2, y2) {
+		t.Errorf("adding by zero did not return original point")
+		return
+	}
+
+}
+
+func TestCurveInverse(t *testing.T) {
+
+	x0, y0 := curve.invertPoint(gx, gy)
+	x0i, y0i := curve.invertPoint(x0, y0)
+
+	if !curve.PointEquals(x0i, y0i, gx, gy) {
+		t.Errorf("inverting the inverse did not results in the same point")
+		return
+	}
+}
+
+func TestCurveDouble(t *testing.T) {
+
+	gx2, gy2 := curve.Double(gx, gy)
+	gxi, gyi := curve.invertPoint(gx, gy)
+	x2, y2 := curve.Add(gx2, gy2, gxi, gyi)
+
+	if !curve.PointEquals(x2, y2, gx, gy) {
+		t.Errorf("point subtraction did not return base point")
+		return
+	}
+
+	x3, y3 := curve.Add(gx, gy, gxi, gyi)
+	if !curve.PointEquals(x3, y3, zero, one) {
+		t.Errorf("addition of inverse did not return zero point")
+		return
+	}
+}
